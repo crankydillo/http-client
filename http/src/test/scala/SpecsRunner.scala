7da89c26ff078ba57599b2d95583f52f3d7c86e2
scox <crankydillo@gmail.com>
2012-03-01 04:11:25 -0600

    Delete modules.

--- http/src/test/scala/SpecsRunner.scala
+++
@@ -1,7 +0,0 @@
-package org.beeherd.http
-
-import org.specs._
-import org.specs.runner.JUnit4
-import org.specs.runner.SpecsFileRunner
-
-object SpecsRunner extends SpecsFileRunner("**/*Spec.scala", ".*")
